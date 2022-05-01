5d88c9906d3f87a87ddb4bb0ac5aa5d278dad4db
Vedanth Bora <vedanthbora@gmail.com>
2022-05-02 03:43:57 +0530

    [FEAT]: hero and banner components and made responsive home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import { Footer, Header } from "./components";
+import { useTheme } from "./context";
 
 import { WebsiteRoutes } from "./routes";
 
 const App = () => {
+  const { theme } = useTheme();
+
   return (
-    <div>
+    <div
+      style={{ backgroundColor: `${theme === "light" ? "white" : "#191A19"}` }}
+    >
       <Header />
       <WebsiteRoutes />
       <Footer />
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { BannerContainer, HeroContainer } from "../components";
 
 const Home = () => {
-  return <div style={{ minHeight: "90vh" }}>ggggg</div>;
+  return (
+    <div style={{ minHeight: "90vh", top: "3rem" }} className="relative">
+      <HeroContainer />
+
+      <BannerContainer />
+
+      <div className="spacer-3rem"> </div>
+      <div className="spacer-3rem"> </div>
+    </div>
+  );
 };
 
 export default Home;
