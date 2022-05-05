cbd95b93d0ef9b9fbda40ab760f7b163929fbda6
Vedanth Bora <vedanthbora@gmail.com>
2022-05-06 01:52:38 +0530

    [FEAT]: text area component

---
+++ src/components/global/TextArea.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const TextArea = ({ errorMessage, label, value, onChange, ...rest }) => {
+  return (
+    <div className="input-container text-black">
+      <textarea
+        type="text"
+        className="input-outlined input-outlined-green h-20 text-black"
+        autoComplete="off"
+        placeholder=" "
+        value={value}
+        onChange={onChange}
+        {...rest}
+      />
+      <label
+        style={{ backgroundColor: "white" }}
+        htmlDor="email"
+        className="input-label text-black"
+      >
+        {label} *
+      </label>
+
+      <small style={{ top: "6.5rem" }} className="error text-red-500 text-sm">
+        {" "}
+        {errorMessage}{" "}
+      </small>
+    </div>
+  );
+};
+
+export default TextArea;
